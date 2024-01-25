c040f0c1d663d66a377e11d13b71ed8c838335e9
srushti patel <srushtipatel2643@gmail.com>
2024-01-25 09:01:41 +0530

    25-01-2024

---
+++ MongoDB/books_query_copy.js
@@ -0,0 +1,13 @@
+// db.books.find({})
+  
+// db.books.find({"pages" : {$eq: 320}})
+// db.books.find({"pages" : {$ne: 320}})
+
+// db.books.find({"pages" : {$gt: 500}})
+// db.books.find({"pages" : {$gte: 720}})
+
+// db.books.find({"pages" : {$lt: 320}})
+// db.books.find({"pages" : {$lte: 320}})
+
+// db.books.find({"pages" : {$in:[320,505,288]}})
+// db.books.find({"pages" : {$nin:[320,505,288]}})
