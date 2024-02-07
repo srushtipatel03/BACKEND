32e47f81248f69838fef5884f5775b89f8b3b6e3
srushti patel <srushtipatel2643@gmail.com>
2024-02-07 09:33:39 +0530

    07-02-2024

---
+++ TypeScript/1_basic.js
@@ -0,0 +1,19 @@
+// let a=10;
+// console.log(a);
+// let a: number = 10;
+// console.log(a);
+// let b='hello'
+// console.log(b);
+// let x : number | string = 100;
+// console.log(x);
+// x='hello'
+// let a: string[] = ["hello"]
+// // let a : Array<string> = ["hello"]
+// a[1] = 'skill'
+// a[20] = '100'
+// console.log(a)
+var x;
+x = ['hello', 10, 20, 'world'];
+x.push("qode", 20);
+x.push(20, "srushti");
+console.log(x);
